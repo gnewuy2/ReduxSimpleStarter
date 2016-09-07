de5c1dcdd87c203c6011ed34124ee6796c03635e
gnewuy2 <gnewuy@gmail.com>
2016-09-07 00:50:37 -0500

    Included an input change event handler in search_bar class

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,7 +13,15 @@ class SearchBar extends Component { // has all functionalities of the React.Comp
 	// each class must have a render() method
 	render() {
 		// must return some jsx
-		return <input />;
+		return <input onChange = {this.onInputChange} />;
+		// reference to the event handler by this. whenever Change event happens
+		// so whenever input is changed, the handler executes.
+	}
+
+	// event handler
+
+	onInputChange(newInput) {
+		console.log(newInput.target.value);
 	}
 }
 
