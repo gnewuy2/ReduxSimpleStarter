aa5d57601fe143cbd341ae04a238ca71fffaec62
gnewuy2 <gnewuy@gmail.com>
2016-09-06 22:38:17 -0500

    Boiler plate commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// Question: How does this tie to the whole application
+// e.g. index.html, app.js?
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component. This component should produce some HTML
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// const is an ES6 syntax; the below <div></div> is called JSX, but 
+// is actually javascript behind the scenes.
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// JSX: cannot be interpreted by the browser directly.
+// it actually produces actual HTML, which gets rendered on the
+// page; JSX gets transpiled.
+
+// paste below lines of code into babeljs.io/repl: we'll see 
+// the interpetation on the right to a React.createElement("div", null, "Hi")
+// e.g.
+// const App = function() {
+// 	return <ol>
+// 	<li>1</li>
+// 	<li>2</li>
+// 	</ol>
+// }
+
+
+const App = function() {
+	return <div>Hi</div>;
+}
+
+React.render(App);
+// Take this component's generated HTML and put it on the page
+// (in the DOM)
